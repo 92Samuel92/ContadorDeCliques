da537cb08fe8c4eb23e6119cfe8835e77901dd90
92Samuel92 <81479102+92Samuel92@users.noreply.github.com>
2021-04-16 18:47:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var contador = document.querySelector('.badge');
-
 document.querySelector('button').addEventListener('click', function(){
   var numero = parseInt(contador.textContent) + 1;
   contador.textContent = numero;
